e681cfb6d2e7328b1199193e2684d43f394d3419
Felipe Dulcey <fdulcey@unicauca.edu.co>
2023-06-28 12:12:57 -0500

    agregue scroll tabla

--- src/Components/Dashboard/DatosCliente/Cliente/ClienteHabilitados.jsx
+++ src/Components/Dashboard/DatosCliente/Cliente/ClienteHabilitados.jsx
@@ -15,7 +15,7 @@ function ClienteHabilitados({ data }) {
 
             <div className="card-body px-0 pb-2">
               <div className="table-responsive p-0">
-                <table className="table align-items-center mb-0">
+                <table className="table align-items-center mb-0 table-cliente">
                   <tbody>
                     <ClienteHabilitadosItems data={data} />
                   </tbody>
